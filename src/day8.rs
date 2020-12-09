38e85b71ea5f925f409b72a876e6d7d2ca55fea8
Julian Hofer <julianhofer@gnome.org>
2020-12-09 09:57:52 +0100

    Finish day8

--- src/day8.rs
+++ src/day8.rs
@@ -64,7 +64,6 @@ fn acc_of_unique_executions(input: &Vec<Instruction>) -> i32 {
     }
     acc
 }
-
 #[test]
 fn test_part_1_acc_of_unique_executions() {
     let input = "\
@@ -78,6 +77,62 @@ acc +1
 jmp -4
 acc +6";
 
-    let hashmap = parse_input_day8(input);
-    assert_eq!(5, acc_of_unique_executions(&hashmap));
+    let instructions = parse_input_day8(input);
+    assert_eq!(5, acc_of_unique_executions(&instructions));
+}
+
+#[aoc(day8, part2)]
+fn acc_of_fixed_executions(input: &Vec<Instruction>) -> i32 {
+    let instruction_length = input.len() as i32;
+    for (fix_index, instruction) in input.iter().enumerate() {
+        let fixed_operation = match instruction.operation {
+            Operation::Acc => Operation::Acc,
+            Operation::Jmp => Operation::Nop,
+            Operation::Nop => Operation::Jmp,
+        };
+        let mut input = input.clone();
+        input[fix_index] = Instruction::new(fixed_operation, instruction.argument);
+
+        let mut index: i32 = 0;
+        let mut acc = 0;
+        let mut visited_indices = Vec::new();
+        while !visited_indices.contains(&index) {
+            visited_indices.push(index);
+            let Instruction {
+                operation,
+                argument,
+            } = input[index as usize];
+
+            match operation {
+                Operation::Acc => {
+                    acc += argument;
+                    index += 1
+                }
+                Operation::Jmp => index += argument,
+                Operation::Nop => index += 1,
+            };
+            if index == instruction_length {
+                return acc;
+            }
+        }
+    }
+
+    panic!("Could not fix the program.")
+}
+
+#[test]
+fn test_part_1_acc_of_fixed_executions() {
+    let input = "\
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+
+    let instructions = parse_input_day8(input);
+    assert_eq!(8, acc_of_fixed_executions(&instructions));
 }
