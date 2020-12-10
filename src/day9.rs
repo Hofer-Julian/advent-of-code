f349eddf60235906b083cd3fcb6e82cd3a6f612f
Julian Hofer <julianhofer@gnome.org>
2020-12-10 20:51:38 +0100

    Finish day9

--- src/day9.rs
+++ src/day9.rs
@@ -10,27 +10,26 @@ fn parse_input_day9(input: &str) -> Vec<usize> {
 }
 
 #[aoc(day9, part1)]
-fn first_number_without_property(input: &Vec<usize>) -> usize {
-    'outer: for (index_number, number) in input.iter().enumerate() {
-        if index_number < 25 {
-            continue;
-        }
-        for (index_first, first_addend) in input.iter().enumerate() {
-            if index_first >= index_number {
-                break;
-            }
-            for (index_second, second_addend) in input.iter().enumerate() {
-                if index_first >= index_second || index_second >= index_number {
-                    break;
-                }
-                if *number == *first_addend + *second_addend {
+fn first_number_no_sum_specific(input: &Vec<usize>) -> usize {
+    first_number_no_sum_general(input, 25)
+}
+
+fn first_number_no_sum_general(input: &Vec<usize>, number: usize) -> usize {
+    let total_size = input.len();
+    'outer: for index_number in number..total_size {
+        for index_first in (index_number - number)..index_number {
+            for index_second in index_first..index_number {
+                if input[index_number] == input[index_first] + input[index_second] {
                     continue 'outer;
                 }
             }
         }
-        return *number;
+        return input[index_number];
     }
-    panic!("Could not find number which is not the sum of two of the 25 numbers before it")
+    panic!(
+        "Could not find number which is not the sum of two of the {} numbers before it",
+        number
+    )
 }
 
 #[test]
@@ -58,5 +57,56 @@ fn test_part_1_first_number_without_property() {
 576";
 
     let instructions = parse_input_day9(input);
-    assert_eq!(127, first_number_without_property(&instructions));
+    assert_eq!(127, first_number_no_sum_general(&instructions, 5));
+}
+
+#[aoc(day9, part2)]
+fn contiguous_set_specific(input: &Vec<usize>) -> usize {
+    contiguous_set_general(input, 36845998)
+}
+
+fn contiguous_set_general(input: &Vec<usize>, number: usize) -> usize {
+    let total_size = input.len();
+    for index_lower in 0..total_size {
+        let mut sum = 0;
+        for index_upper in index_lower..total_size {
+            sum += input[index_upper];
+            if sum == number {
+                let range: Vec<usize> = input[index_lower..=index_upper].into();
+                return range.iter().min().unwrap() + range.iter().max().unwrap();
+            }
+        }
+    }
+    panic!(
+        "Could not find a contiguous set of at least two numbers which sum to {}",
+        number
+    )
+}
+
+#[test]
+fn test_part_2_contiguous_set() {
+    let input = "\
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+    let instructions = parse_input_day9(input);
+    assert_eq!(62, contiguous_set_general(&instructions, 127));
 }
