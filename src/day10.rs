c332e4ab0ff23a486ec3112eeca347667bbe7d06
Julian Hofer <julianhofer@gnome.org>
2020-12-10 21:37:48 +0100

    First attempt for day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,3 +1,5 @@
+use std::ops::Div;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day10)]
@@ -78,6 +80,62 @@ fn test_part_1_get_differences_multiplied() {
 }
 
 #[aoc(day10, part2)]
-fn total_number_of_arrangments(input: &[usize]) -> usize {
-    unimplemented!();
+fn get_total_number_of_arrangments(input: &[usize]) -> usize {
+    let mut sorted_input = input.clone().to_vec();
+    sorted_input.push(0);
+    sorted_input.sort();
+
+    get_number_of_arrangments(&sorted_input)
+}
+
+fn get_number_of_arrangments(input: &[usize]) -> usize {
+    let mut number_of_arrangments = 0;
+    if input.len() == 1 {
+        return 1;
+    }
+    for difference in 1..=3 {
+        if input[0] + difference == input[1] {
+            number_of_arrangments += get_number_of_arrangments(&input[1..])
+        }
+    }
+    number_of_arrangments
+}
+
+#[test]
+fn test_part_2_get_total_number_of_arrangments() {
+    let input = "\
+28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+
+    let instructions = parse_input_day10(input);
+    assert_eq!(19208, get_total_number_of_arrangments(&instructions));
 }
