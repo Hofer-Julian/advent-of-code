3d9949d3ccc464bd8359a9bb0e3d8d11ecdad15f
Julian Hofer <julianhofer@gnome.org>
2020-12-14 10:54:49 +0100

    Add ansatz day14

---
+++ src/day14.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day14)]
+fn parse_input_day14(input: &str) -> (u64, u64, Vec<(u64, u64)>) {
+    todo!()
+}
+
+#[aoc(day14, part1)]
+fn get_answer_day14_part1(input: &(u64, u64, Vec<(u64, u64)>)) -> u64 {
+    todo!()
+}
