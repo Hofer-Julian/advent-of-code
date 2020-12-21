1e854550572d2d18b8debf1501930118867d0ef3
Julian Hofer <julianhofer@gnome.org>
2020-12-21 18:11:28 +0100

    Further ansatz for day14 part2

--- src/day14_part2.rs
+++ src/day14_part2.rs
@@ -3,24 +3,9 @@ use std::{collections::HashMap, todo};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
-#[derive(Debug, Default)]
-struct Program {
-    bitmask: Vec<char>,
-    instruction: HashMap<u64, u64>,
-}
-
-impl Program {
-    fn new(bitmask: Vec<char>, write_instruction: HashMap<u64, u64>) -> Self {
-        Self {
-            bitmask,
-            instruction: write_instruction,
-        }
-    }
-}
-
 #[aoc_generator(day14, part2)]
 fn parse_input_day14_part2(input: &str) -> Vec<Program> {
-    let mut program;
+    let mut program = Program::default();
 
     let mut parsed_data = Vec::new();
     let re = regex!(r"mem\[(\d+)\] = (\d+)");
@@ -29,7 +14,7 @@ fn parse_input_day14_part2(input: &str) -> Vec<Program> {
         if let Some(captures) = re.captures(line) {
             let position = captures.get(1).unwrap().as_str().parse().unwrap();
             let value = captures.get(2).unwrap().as_str().parse().unwrap();
-            program.instruction.insert(position, value);
+            program.write_instructions.insert(position, value);
         } else {
             parsed_data.push(program);
             program = Program::default();
@@ -42,7 +27,50 @@ fn parse_input_day14_part2(input: &str) -> Vec<Program> {
     parsed_data
 }
 
+#[derive(Debug, Default)]
+struct Program {
+    bitmask: Vec<char>,
+    write_instructions: HashMap<u64, u64>,
+}
+
 #[aoc(day14, part2)]
 fn sum_values_in_memory(input: &Vec<Program>) -> u64 {
+    let actual_write_instruction = get_actual_write_instruction(input);
+    actual_write_instruction
+        .iter()
+        .fold(0, |acc, (_, value)| acc + value)
+}
+
+fn get_actual_write_instruction(input: &Vec<Program>) -> HashMap<u64, u64> {
+    input.iter().flat_map(get_single_mask).collect()
+}
+fn get_single_mask(program: &Program) -> HashMap<u64, u64> {
+    program
+        .write_instructions
+        .iter()
+        .flat_map(|(memory_address, value)| {
+            get_single_write_instruction(&program.bitmask, memory_address, value)
+        })
+        .collect()
+}
+
+fn get_single_write_instruction(
+    bitmask: &Vec<char>,
+    memory_address: &u64,
+    value: &u64,
+) -> HashMap<u64, u64> {
     todo!()
+}
+
+#[cfg(test)]
+#[test]
+fn test_sum_values_in_memory() {
+    let input = "\
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1";
+
+    let instructions = parse_input_day14_part2(input);
+    assert_eq!(208, sum_values_in_memory(&instructions));
 }
