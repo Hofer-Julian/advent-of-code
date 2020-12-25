7502a8dd2cba2ee74727fd733c63d01021232c63
Julian Hofer <julianhofer@gnome.org>
2020-12-26 00:04:06 +0100

    Finish day16 part1

--- 2020/16-25_julia/src/day16.jl
+++ 2020/16-25_julia/src/day16.jl
@@ -1,7 +1,57 @@
 module day16
 
+function read_file()
+    lines = open("$(@__DIR__)/../input/day16.txt") do f
+        input = read(f, String)
+        parse_file(input)
+    end
+end
+
+function parse_file(input::AbstractString)
+    input = strip(input)
+    rules, mine, nearby = split(input, "\n\n")
+    split(rules, "\n"), split(mine, "\n"), split(nearby, "\n") 
+end
+
+
+function ticket_error_rate(rules, nearby)
+    ranges = Set()
+    for rule in rules
+        m = match(r".*: (\d+)-(\d+) or (\d+)-(\d+)", rule)
+
+        
+        range_1 = (lower = parse(Int, m[1]), upper = parse(Int, m[2]))
+        range_2 = (lower = parse(Int, m[3]), upper = parse(Int, m[4]))
+
+        push!(ranges, range_1)
+        push!(ranges, range_2)
+    end
+
+    error_rate = 0
+    for fields in nearby
+        if fields == "nearby tickets:"
+            continue
+        end
+        fields_array = split(fields, ",")
+        for field in fields_array
+            field = parse(Int, field)
+            for range in ranges
+                if range.lower <= field <= range.upper
+                    @goto field_loop
+                end
+            end
+            error_rate += field
+            @label field_loop
+        end    
+    end
+    error_rate
+end
+
+
+
 function run()
-    println("Hallo")    
+    rules, mine, nearby = read_file()
+    println("The answer to the first part is $(ticket_error_rate(rules, nearby))") 
 end
 
 end
--- 2020/16-25_julia/test/test_day16.jl
+++ 2020/16-25_julia/test/test_day16.jl
@@ -2,5 +2,20 @@ using Test
 include("../src/day16.jl")
 
 @testset "Error rate" begin
-    @test 1 + 1 == 2
+    input = """
+class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"""
+
+    rules, mine, nearby = day16.parse_file(input)
+    @test day16.ticket_error_rate(rules, nearby) == 71
 end
