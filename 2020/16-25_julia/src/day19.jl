569c2c38b2594381ee0c98d0ce50c8cca2ace908
Julian Hofer <julianhofer@gnome.org>
2020-12-28 23:01:23 +0100

    Intermediate state of day19

--- 2020/16-25_julia/src/day19.jl
+++ 2020/16-25_julia/src/day19.jl
@@ -1,10 +1,12 @@
 module day19
 
+
 function read_file()
     lines = open("$(@__DIR__)/../input/day19.txt") do f
         read(f, String) |> chomp        
     end
 end
+
 
 function parse_input(input)
     rules, messages = split(input, "\n\n")
@@ -12,9 +14,90 @@ function parse_input(input)
 end
 
 
+function number_match_rule_zero(rules, messages)
+    working_dict, finished_dict = get_starting_dicts(rules)
+
+    while true
+        changed = false
+
+    end
+end
+
+
+function finished_dict_contains_numbers(finished_dict, numbers)
+    for first_dim in numbers
+        for number in first_dim
+            if number ∉ keys(finished_dict)
+                return false
+            end
+        end
+    end
+    true
+end
+
+
+function get_starting_dicts(rules)
+    re_general = r"(\d+): (.*)"
+    re_inclusive = r"(\d+) (\d+) \| (\d+) (\d+)"
+    re_one = r"(\d+)"
+    re_two = r"(\d+) (\d+)"
+    re_three = r"(\d+) (\d+) (\d+)"
+    re_letter = r"\"(\w)\""
+
+    finished_dict = Dict{Int,Any}()
+    working_dict = Dict{Int,Any}()
+
+    
+    for rule in rules
+        m = match(re_general, rule)
+        if m === nothing
+            throw(ErrorException("This rule does not match the basic pattern: $rule"))
+        end
+        index = parse(Int, m.captures[1])
+        context = m.captures[2]
+        
+
+        m = match(re_letter, context)
+        if m !== nothing
+            finished_dict[index] = [m.captures[1]]
+            continue
+        end
+
+        m = match(re_inclusive, context)
+        if m !== nothing
+            working_dict[index] = [[m.captures[1], m.captures[2]],
+                                   [m.captures[3], m.captures[4]]]
+            continue
+        end
+
+        m = match(re_three, context)
+        if m !== nothing
+            working_dict[index] = [[m.captures[1], m.captures[2], m.captures[3]]]
+            continue
+        end
+
+        m = match(re_two, context)
+        if m !== nothing
+            working_dict[index] = [[m.captures[1], m.captures[2]]]
+            continue
+        end
+
+
+        m = match(re_one, context)
+        if m !== nothing
+            working_dict[index] = [[m.captures[1]]]
+            continue
+        end
+
+        throw(ErrorException("This rule could not be parsed: $rule"))
+
+    end
+    working_dict, finished_dict
+end
+
 function run()
-    input = read_file() |> parse_input
-    println("The answer to the first part is $input")
+    rules, messages = read_file() |> parse_input
+    println("The answer to the first part is")
 end
 
 end
--- 2020/16-25_julia/test/test_day19.jl
+++ 2020/16-25_julia/test/test_day19.jl
@@ -2,7 +2,7 @@ using Test
 include("../src/day19.jl")
 
 @testset "Day19 part1: Number of messages matching rule 0" begin
-    example_1 = """"
+    example_1 = """
 0: 4 1 5
 1: 2 3 | 3 2
 2: 4 4 | 5 5
@@ -16,7 +16,7 @@ abbbab
 aaabbb
 aaaabbb"""
 
-    input = day19.parse_input(example_1)
-    @test day19.number_match_rule_zero(input) == 2
+    rules, messages = day19.parse_input(example_1)
+    @test day19.number_match_rule_zero(rules, messages) == 2
     
 end
